1e27835b22d15da2c67e5b3dfce1519a2eabca24
ouzdeville <ouzdeville@gmail.com>
2021-04-08 18:49:43 +0000

    commentaire

---
+++ essa2021/src/java/controller/UserManagedBean.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author OUSMANE
+ */
+@ManagedBean
+@SessionScoped
+public class UserManagedBean {
+
+    /**
+     * Creates a new instance of UserManagedBean
+     */
+    public UserManagedBean() {
+    }
+    
+    private String login;
+    private String password;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+}
